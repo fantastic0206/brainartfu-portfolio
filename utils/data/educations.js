b38fc2a52bb81b63fccce8b9696eb74fdd8b8011
fantastic0206 <fatastic20201110@outlook.com>
2024-06-17 05:46:12 -0700

    brainartfu portfolio commit

---
+++ utils/data/educations.js
@@ -0,0 +1,8 @@
+export const educations = [
+  {
+    id: 1,
+    title: "Bachelor's degree, Informatics",
+    duration: "2019 - Present",
+    institution: "Universitatea Ovidius Constanta",
+  }
+]
---
+++ utils/data/personal-data.js
@@ -0,0 +1,17 @@
+export const personalData = {
+  name: "Zbrulea Constantin",
+  profile: '/profile.jpg',
+  designation: "Software Developer",
+  description: "My name is Zbrulea Constantin. I am a highly skilled and experienced software developer with a deep understanding of software design, development, and maintenance. I am proficient in multiple programming languages and technologies, and I have a proven track record of delivering high-quality software solutions. I am a strong team player and I am always eager to learn new things. Ultimately, the goal is to create software that has a positive impact on the world, solving problems, increasing efficiency, and making people's lives easier. I am confident that I can make a significant contribution to your organization.",
+  email: 'brainartfu@gmail.com',
+  phone: '+40 7*******7',
+  address: 'Prislav nr.51, Tulcea, TL, Romania',
+  github: 'https://github.com/zbrdev',
+  linkedIn: 'https://www.linkedin.com/in/zbrulea-constantin-6b3241311/',
+  stackOverflow: 'https://stackoverflow.com/users/25537821/zbr',
+  facebook: 'https://www.facebook.com/zbr.constantin/',
+  twitter: 'https://twitter.com/zbrconstatin',
+  leetcode: "https://leetcode.com/u/BrainArtfu/",
+  devUsername: "BrainArtfu",
+  resume: "https://drive.google.com/file/d/1WKEBIECCZd6y3qXM8XvCjZ3LQ0D6G3PG/view?usp=sharing"
+}
